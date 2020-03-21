3842ce4005de6f095a521c00ed43bc5a015d6333
Ahmad Rais <ahmad.rdj2@gmail.com>
2020-03-21 16:39:49 +0700

    Program atau object

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObject
+{
+    public class Mahasiswa
+    {
+        //properties
+        public string NIM { get; set; }
+        public string Nama { get; set; }
+        public float IPK { get; set; }
+        
+        //method
+        public void Regristrasi()
+        {
+            Console.WriteLine("Nama : {0}", Nama);
+            Console.WriteLine("NIM  : {0}", NIM);
+            Console.WriteLine("IPK  : {0}", IPK);
+            Console.WriteLine("\nTelah melakukan regristrasi");
+        }
+        public void IsiKRS()
+        {
+            Console.WriteLine("{0} Sedang melakukan proses KRS", Nama);
+        }
+    }
+}
